2390927b14139a7484ce0924661e8fcfadfd825e
alamin209 <alamin20192019@gmail.com>
2024-02-27 08:19:29 +0600

    Push the code

---
+++ backend/models/userModel.js
@@ -0,0 +1,40 @@
+const userSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+    username:{
+        type:String,
+        required: true,
+        unique: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String
+    },
+    profilePic:{
+        type:String,
+        default:''
+    },
+    followers:{
+        type:[String],
+        default:[]
+    },
+    followings:{
+        type:[String],
+        default:[]
+    },
+    bio:{
+        type: String,
+        default:""
+    }
+
+},{
+    timestamps: true,
+})
+const User =mongoose.model("User",userSchema);
+export default User;
